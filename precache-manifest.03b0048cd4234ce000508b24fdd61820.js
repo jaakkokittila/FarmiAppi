6a6eab42afeba63a6c8343b08400ec413c57c1ea
Jaakko Kittilä <jmkitt@utu.fi>
2020-01-25 22:53:11 +0200

    Updates

---
+++ precache-manifest.03b0048cd4234ce000508b24fdd61820.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe9aea954ac3d357cb8c0b3c8c0315a",
+    "url": "/FarmiAppi/index.html"
+  },
+  {
+    "revision": "d08f63dc1d93b096bb80",
+    "url": "/FarmiAppi/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5271f40c4370b4a2a5d3",
+    "url": "/FarmiAppi/static/css/main.eb0149a0.chunk.css"
+  },
+  {
+    "revision": "d08f63dc1d93b096bb80",
+    "url": "/FarmiAppi/static/js/2.c559b6d6.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/FarmiAppi/static/js/2.c559b6d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5271f40c4370b4a2a5d3",
+    "url": "/FarmiAppi/static/js/main.f761b3f4.chunk.js"
+  },
+  {
+    "revision": "957530d1e1bede68df77",
+    "url": "/FarmiAppi/static/js/runtime-main.27534b2f.js"
+  },
+  {
+    "revision": "57982727f16b77d7314d00b7a4ece937",
+    "url": "/FarmiAppi/static/media/header.57982727.jpg"
+  }
+]);
