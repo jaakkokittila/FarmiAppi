20050a842fb8a472d8017e44ff056a68f4d79715
Jaakko Kittilä <jmkitt@utu.fi>
2020-01-25 23:40:18 +0200

    Paranneltu tuntien asettamista

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return(
     <div id="app">
       <Header />
-      <div id="formdiv">
+      <div>
         <PriceForm />
       </div>
       
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import { Alert } from 'react-bootstrap'
 
 const PriceForm = () => {
     const [fertilizer, setFertilizer] = useState('Superlannoite')
@@ -9,10 +10,23 @@ const PriceForm = () => {
     const [form, setForm] = useState(true)
     const [berries, setBerries] = useState(0)
     const [costs, setCosts] = useState(0)
+    const [error, setError] = useState(null)
 
     const handleSubmit = (e) => {
        e.preventDefault()
        
+       if(hours > 120 || hours < 20){
+            setError(
+            <Alert variant="danger" className="alert">
+                Tuntien pitää olla välillä 20-120!
+            </Alert>)
+
+            setTimeout(() => {
+                setError(null)
+            }, 5000)
+
+            return
+       }
 
         if(fertilizer === 'Superlannoite'){
             setCosts(2500 + lights * 45 + hours * 25)
@@ -30,49 +44,49 @@ const PriceForm = () => {
 
     if (form){
         return (
-            <Form className="form" onSubmit={handleSubmit}>
-                <Form.Label>Fertilizer: </Form.Label>
-                <Form.Control size="lg" value={fertilizer} as="select" onChange={(event) => setFertilizer(event.target.value)}>
-                    <option>Superlannoite</option>
-                    <option>Tavallinen lannoite</option>
-                    <option>Lirulannoite</option>
-                </Form.Control>
-                <br />
-                <Form.Label>Valaisimien määrä: </Form.Label>
-                <Form.Control size="lg" value={lights} as="select" onChange={(event) => setLights(event.target.value)}>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                    <option>7</option>
-                    <option>8</option>
-                    <option>9</option>
-                    <option>10</option>
-                </Form.Control>
-                <br />
-                <Form.Label>Työtuntien määrä: </Form.Label>
-                <Form.Control size="lg" as="input" type="number" value={hours} onChange={(event) => {
-                    if (event.target.value < 20){
-                        setHours(20)
-                    }else if (event.target.value > 120){
-                        setHours(120)
-                    }else{
-                        setHours(event.target.value)}
-                    }
-                    
-                    }></Form.Control>
-                <br />
-                <Button variant="primary" type="submit">Laske kustannukset</Button>
-            </Form>)
+            <div>
+                {error}
+                <div id="formdiv">
+               
+                    <Form className="form" onSubmit={handleSubmit}>
+                        <Form.Label>Fertilizer: </Form.Label>
+                        <Form.Control size="lg" value={fertilizer} as="select" onChange={(event) => setFertilizer(event.target.value)}>
+                            <option>Superlannoite</option>
+                            <option>Tavallinen lannoite</option>
+                            <option>Lirulannoite</option>
+                        </Form.Control>
+                        <br />
+                        <Form.Label>Valaisimien määrä: </Form.Label>
+                        <Form.Control size="lg" value={lights} as="select" onChange={(event) => setLights(event.target.value)}>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                        </Form.Control>
+                        <br />
+                        <Form.Label>Työtuntien määrä: </Form.Label>
+                        <Form.Control size="lg" as="input" type="number" value={hours} onChange={(event) => setHours(event.target.value)}></Form.Control>
+                        <br />
+                        <Button variant="primary" type="submit">Laske kustannukset</Button>
+                    </Form>
+                </div>
+            </div>
+            )
     }else {
         return(
-            <div className="results">
-                <h1>Arvioidut kustannukset</h1>
-                <p>Näillä arvoilla marjoja tulee {berries} kg, {costs}€ hintaan.</p>
-                <p>Minimikilohinnaksi muodostuu siis <b>{costs / berries}€/kg</b> </p>
-                <Button variant="primary" onClick={() => setForm(true)}>Laske uusi</Button>
+            <div id="formdiv">
+                <div className="results">
+                    <h1>Arvioidut kustannukset</h1>
+                    <p>Näillä arvoilla marjoja tulee {berries} kg, {costs}€ hintaan.</p>
+                    <p>Minimikilohinnaksi muodostuu siis <b>{costs / berries}€/kg</b> </p>
+                    <Button variant="primary" onClick={() => setForm(true)}>Laske uusi</Button>
+                </div>
             </div>
         )
     }
